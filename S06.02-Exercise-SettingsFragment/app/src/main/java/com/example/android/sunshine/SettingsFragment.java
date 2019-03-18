4668475d5ff5b40f1a4948d6750fdac19516328d
Hasan El-Hefnawy <helhefnawy89@yahoo.com>
2019-03-18 13:43:53 +0200

    S06.02-Exercise-SettingsFragment

---
+++ S06.02-Exercise-SettingsFragment/app/src/main/java/com/example/android/sunshine/SettingsFragment.java
@@ -0,0 +1,57 @@
+package com.example.android.sunshine;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.preference.CheckBoxPreference;
+import android.support.v7.preference.EditTextPreference;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.PreferenceScreen;
+
+public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.preference);
+        PreferenceScreen prefScreen = getPreferenceScreen();
+        int count = prefScreen.getPreferenceCount();
+        for (int i = 0; i < count; i++) {
+            Preference p = prefScreen.getPreference(i);
+            if (!(p instanceof CheckBoxPreference)) {
+                String value = prefScreen.getSharedPreferences().getString(p.getKey(), "");
+                setPreferenceSummary(p, value);
+            }
+        }
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Preference pref = findPreference(key);
+        if (!(pref instanceof CheckBoxPreference)) {
+            String value = sharedPreferences.getString(key, "");
+            setPreferenceSummary(pref, value);
+        }
+    }
+
+    public void setPreferenceSummary (Preference preference, String value) {
+        if (preference instanceof ListPreference) {
+            ListPreference listPref = (ListPreference) preference;
+            int index = listPref.findIndexOfValue(value);
+            listPref.setSummary(listPref.getEntries()[index]);
+        } else if (preference instanceof EditTextPreference) {
+            preference.setSummary(value);
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+    }
+}
